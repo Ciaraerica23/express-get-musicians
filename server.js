b4be8cf0a7b83a7d0822e2d5e01d057f2152c5b0
Ciaraerica23 <ciaraestafford@gmail.com>
2023-03-10 12:49:45 -0500

    added crud methods

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ app.get('/musicians/:id',async(req,res)=>{
     res.json(found)
 })
 
+app.post('/musicians',async(req,res)=>{
+   await Musician.create({name:"Sza",
+    instrument:"voice"})
+    const found1 = await Musician.findAll()
+    res.send(found1)
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.post('/musicians/add',async(req,res)=>{
+    await Musician.create(req.body)
+     const found2 = await Musician.findAll()
+     res.send(found2)
+ })
+
+ app.delete('/musicians/:id',async(req,res)=>{
+    const id = req.params.id
+    const found = await Musician.findByPk(id)
+    await found.destroy()
+    const found2 = await Musician.findAll()
+    res.json(found2)
+ })
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
